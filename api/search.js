dbe63f7b3a6848a60beff6bbe7c2133c8e90a15f
Ricky John Garciano Reusora <175661621+akirachoi01@users.noreply.github.com>
2025-07-01 04:11:37 +0800

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
-  const id = req.headers['cf-access-client-id'];
-  const secret = req.headers['cf-access-client-secret'];
+  const id = req.headers['CF-Access-Client-Id: fc60d40c98172111509f2c371a20b988.access'];
+  const secret = req.headers['3bd763913e1d88115a341fb19d0cfe673c55a76379de67b472ab3cf94aeef472'];
   if (!id || !secret) {
     return res.status(401).json({ error: 'Unauthorized' });
   }
